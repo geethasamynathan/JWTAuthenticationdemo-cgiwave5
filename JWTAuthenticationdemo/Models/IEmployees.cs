a69b186c39cebc61ee27d505a1f5903f6f05a8a1
geethasamynathan <geethasamynathan2011@gmail.com>
2022-10-10 15:08:38 +0530

    Add project files.

---
+++ JWTAuthenticationdemo/Models/IEmployees.cs
@@ -0,0 +1,12 @@
+﻿namespace JWTAuthenticationdemo.Models
+{
+    public interface IEmployees
+    {
+        public List<Employee> GetEmployeeDetails();
+        public Employee GetEmployeeDetails(int id);
+        public void AddEmployee(Employee employee);
+        public void UpdateEmployee(Employee employee);
+        public Employee DeleteEmployee(int id);
+        public bool CheckEmployee(int id);
+    }
+}
